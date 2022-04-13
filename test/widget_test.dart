5675a4068e05d971cb53f0c8482dc5b34896a8bd
penaa9 <trivena.natalia2015@gmail.com>
2022-04-13 15:02:46 +0700

    commit looping

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -106,6 +106,34 @@ void main() {
   // memaksa untuk dijalankan / yakin ada datanya
   print(jurusanns!.length);
 
+  //perulangan looping
+  // for plus
+  for(int no = 1; no<= 5; no++){
+    print(no);
+  }
+  //for minus
+  for(int no = 5; no>= 1; no--){
+    print(no);
+  }
+  
+  //while
+  int no1 = 1;
+  int no2 = 5;
+  while(no1 <= no2) {
+    print(no1);
+    no1++;
+  }
+  
+  //do while
+  int no3 = 1;
+  int no4 = 5;
+  do{
+    print(no3);
+    no3++; 
+  }while(no3 <= no4);
+}
+
+
   //Function
   print("Function");
   hitungNilai();
